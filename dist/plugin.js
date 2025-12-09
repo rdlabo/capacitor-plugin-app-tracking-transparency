3a14c1dc91e62d79f76a99a01b1e8d179b07d326
rdlabo <sakakibara@rdlabo.jp>
2025-12-09 17:01:09 +0900

    chore(): add dist for git

---
+++ dist/plugin.js
@@ -0,0 +1,27 @@
+var capacitorAppTrackingTransparency = (function (exports, core) {
+    'use strict';
+
+    const AppTrackingTransparency = core.registerPlugin('AppTrackingTransparency', {
+        web: () => Promise.resolve().then(function () { return web; }).then((m) => new m.AppTrackingTransparencyWeb()),
+    });
+
+    class AppTrackingTransparencyWeb extends core.WebPlugin {
+        getStatus() {
+            return Promise.resolve({ status: 'authorized' });
+        }
+        requestPermission() {
+            return Promise.resolve({ status: 'authorized' });
+        }
+    }
+
+    var web = /*#__PURE__*/Object.freeze({
+        __proto__: null,
+        AppTrackingTransparencyWeb: AppTrackingTransparencyWeb
+    });
+
+    exports.AppTrackingTransparency = AppTrackingTransparency;
+
+    return exports;
+
+})({}, capacitorExports);
+//# sourceMappingURL=plugin.js.map
